df8aecd7b60f8cf6cd6da9002d4b741a07815973
Vanessa <vanessamaikojones@gmail.com>
2020-09-22 11:51:51 -0800

    grayboxed w/ basic player movement

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject platformPrefab;
+    private GameObject newPlatform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        newPlatform = (GameObject)Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.5f, 1.0f))), Quaternion.identity);
+        Destroy(collision.gameObject);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D playerRB2D;
+    private float movementInput;
+    public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        movementInput = Input.GetAxis("Horizontal");
+        playerRB2D.velocity = new Vector2(movementInput * speed, playerRB2D.velocity.y);
     }
 }
